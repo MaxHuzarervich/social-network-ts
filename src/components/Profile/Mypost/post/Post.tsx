354fac8af0671ed26e45d1408ee787074bb1c51e
MaxHuzarevich <pinsk.maks@mail.ru>
2021-04-06 13:48:06 +0300

    comment - 1

--- src/components/Profile/Mypost/post/Post.tsx
+++ src/components/Profile/Mypost/post/Post.tsx
@@ -8,7 +8,7 @@ type PostPropsType = {
 
 function Post(props:PostPropsType) {
     return <div className={s.item}>
-        <img src='https://www.logodesign.net/logo/alt-5607ld.png?size=2&industry=gaming'/>
+        <img src='https://www.logodesign.net/logo/alt-5607ld.png?size=2&industry=gaming'/>    //comment1
         {props.message}
         <div><span>Like {props.Count}</span></div>
     </div>
