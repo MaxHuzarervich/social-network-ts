491bc270212c72ebb8bd3c15a834ddd621a5475d
MaxHuzarevich <pinsk.maks@mail.ru>
2021-04-20 15:09:57 +0300

    const addPost = () => {alert(newPostElement.current?.value)}        // ? - если current существует тогда ->

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -5,11 +5,13 @@ import Message from "./Message/Message";
 import {messagesPageType, messagesType} from '../../redux/state';
 
 type dialogsPropsType = {
-dialogsPage: messagesPageType
+dialogsPage: messagesPageType;
+
 }
 
 function Dialogs(props:dialogsPropsType) {
-
+    let newPostElement = React.createRef<HTMLTextAreaElement>();
+    const addPost = () => {alert(newPostElement.current?.value)}        // ? - если current существует тогда ->
     let dialogsElements = props.dialogsPage.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)       // d = dialogs
     let messageElements = props.dialogsPage.messages.map(m => <Message message={m.message} id={m.id}/>)             // m = messages
     return (
@@ -20,6 +22,8 @@ function Dialogs(props:dialogsPropsType) {
             <div className={s.messages}>
                 {messageElements}
             </div>
+            <button onClick={addPost} style={{backgroundColor:"Violet",borderRadius:'5px'}}>Add new message</button>
+            <textarea ref={newPostElement} style={{borderRadius:'5px'}}></textarea>
         </div>
     )
 }
