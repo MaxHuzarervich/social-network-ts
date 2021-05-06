c6e68b604fe4d2b29a5eca9d509844e903cba79d
MaxHuzarevich <pinsk.maks@mail.ru>
2021-05-06 13:41:01 +0300

    learn 37.1

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -27,47 +27,19 @@ export type profilePageType = {
     posts: Array<postsType>,
     messageForNewPost: string
 }
-export type appStateType = {
-    profilePage: profilePageType,
-    dialogsPage: messagesPageType
-}
-let state: appStateType = {
-    profilePage: {
-        messageForNewPost: '',
-        posts: [
-            {id: 1, message: 'Hi, how are you?', count: 15},
-            {id: 2, message: 'My first post', count: 20},
-            {id: 3, message: 'Blabla', count: 11},
-            {id: 4, message: 'Dada', count: 12}
-        ]
-    },
-    dialogsPage: {
-        messages: [
-            {id: 1, message: 'hi'},
-            {id: 2, message: 'How is your it-kamasutra?'},
-            {id: 3, message: 'Yo'},
-            {id: 4, message: 'Yo'},
-            {id: 5, message: 'Yo'}
-        ],
-        dialogs: [
-            {id: 1, name: 'Dimych'},
-            {id: 2, name: 'Andrey'},
-            {id: 3, name: 'Svetlana'},
-            {id: 4, name: 'Sasha'},
-            {id: 5, name: 'Victor'},
-            {id: 6, name: 'Valera'}
-        ]
-    }
-
-}
-
 export const changeNewText = (newText: string) => {
     state.profilePage.messageForNewPost = newText;
     onChange();
 }
 
+export type appStateType = {
+    profilePage: profilePageType,
+    dialogsPage: messagesPageType
+}
+
+
 export const addPost = (postText: string) => {
-    debugger;                 //функция для создания нового поста
+    //функция для создания нового поста
     const newPost: postsType = {
         id: new Date().getTime(),
         message: postText,
@@ -77,4 +49,40 @@ export const addPost = (postText: string) => {
     onChange();
 }
 
-export default state;
+type storeType = {
+    _state: appStateType
+}
+
+const store: storeType = {
+    _state: {
+        profilePage: {
+            messageForNewPost: '',
+            posts: [
+                {id: 1, message: 'Hi, how are you?', count: 15},
+                {id: 2, message: 'My first post', count: 20},
+                {id: 3, message: 'Blabla', count: 11},
+                {id: 4, message: 'Dada', count: 12}
+            ]
+        },
+        dialogsPage: {
+            messages: [
+                {id: 1, message: 'hi'},
+                {id: 2, message: 'How is your it-kamasutra?'},
+                {id: 3, message: 'Yo'},
+                {id: 4, message: 'Yo'},
+                {id: 5, message: 'Yo'}
+            ],
+            dialogs: [
+                {id: 1, name: 'Dimych'},
+                {id: 2, name: 'Andrey'},
+                {id: 3, name: 'Svetlana'},
+                {id: 4, name: 'Sasha'},
+                {id: 5, name: 'Victor'},
+                {id: 6, name: 'Valera'}
+            ]
+        }
+
+    }
+}
+
+export default store;
