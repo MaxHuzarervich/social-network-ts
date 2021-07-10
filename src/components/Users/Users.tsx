2b5fdaf53e36ae3a814bee98a0803eafcb6dab71
MaxHuzarevich <pinsk.maks@mail.ru>
2021-07-10 22:28:29 +0300

    51 start

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,34 +1,37 @@
 import React from "react";
 import {UsersPropsType} from "./UsersContainer";
+import {Button} from "@material-ui/core";
+import './Users.module.css';
 
-export let Users = (props:UsersPropsType) => {
-    if(props.usersPage.users.length === 0){
-props.setUser ([
-    {
-        id: 1,
-        photoUrl: 'https://files.fm/thumb_show.php?i=w972bbxwg',
-        followed: true,
-        fullName: 'Angelo',
-        status: 'I am programmer',
-        location: {city: 'Sacramento', country: 'USA'}
-    },
-    {
-        id: 2,
-        photoUrl: 'https://files.fm/thumb_show.php?i=w972bbxwg',
-        followed: false,
-        fullName: 'Ivan',
-        status: 'I am music editor',
-        location: {city: 'Moscow', country: 'Russia'}
-    },
-    {
-        id: 3,
-        photoUrl: 'https://files.fm/thumb_show.php?i=w972bbxwg',
-        followed: true,
-        fullName: 'John',
-        status: 'I am superhero!',
-        location: {city: 'Sydney', country: 'Australia'}
-    },
-])}
+export let Users = (props: UsersPropsType) => {
+    if (props.usersPage.users.length === 0) {
+        props.setUser([
+            {
+                id: 1,
+                photoUrl: 'https://image.flaticon.com/icons/png/128/836/836387.png',
+                followed: true,
+                fullName: 'Angelo',
+                status: 'I am programmer',
+                location: {city: 'Sacramento', country: 'USA'}
+            },
+            {
+                id: 2,
+                photoUrl: 'https://image.flaticon.com/icons/png/128/836/836387.png',
+                followed: false,
+                fullName: 'Ivan',
+                status: 'I am music editor',
+                location: {city: 'Moscow', country: 'Russia'}
+            },
+            {
+                id: 3,
+                photoUrl: 'https://image.flaticon.com/icons/png/128/836/836387.png',
+                followed: true,
+                fullName: 'John',
+                status: 'I am superhero!',
+                location: {city: 'Sydney', country: 'Australia'}
+            },
+        ])
+    }
 
 
     // для каждого пользователя вернуть <div>
@@ -36,14 +39,18 @@ props.setUser ([
         {props.usersPage.users.map(u => <div key={u.id}>
         <span>
             <div>
-                <img src={u.photoUrl} />
+                <img src={u.photoUrl}/>
             </div>
             <div>
-                {u.followed ? <button onClick={()=>{props.unfollow(u.id)}}>Unfollow</button> :
-                              <button onClick={()=>{props.follow(u.id)}}>Follow</button>}
+                {u.followed ? <Button color={'default'} onClick={() => {
+                        props.unfollow(u.id)
+                    }}>Unfollow</Button> :
+                    <Button color={'default'} onClick={() => {
+                        props.follow(u.id)
+                    }}>Follow</Button>}
             </div>
         </span>
-        <span>
+            <span>
             <span>
                 <div>{u.fullName}</div>
                 <div>{u.status}</div>
