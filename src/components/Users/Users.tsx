a9f57b150a96543ea6aadc985cd28e8af0e18678
MaxHuzarevich <pinsk.maks@mail.ru>
2021-07-13 15:03:00 +0300

    51 ------->

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,27 +2,25 @@ import React from "react";
 import {UsersPropsType} from "./UsersContainer";
 import {Button} from "@material-ui/core";
 import './Users.module.css';
-import * as axios from 'axios';
-import userPhoto from '../../assets/images/'
+import axios from 'axios';
+import userPhoto from '../../assets/images/user.png'
 
 
 export let Users = (props: UsersPropsType) => {
     if (props.usersPage.users.length === 0) {
-        debugger
+        debugger       //когда выполнишь запрос, выполни затем воооооот этот вот коллбек(response - ответ)
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response=> {
 
             props.setUser(response.data.items)
         });
 
     }
-
-
     // для каждого пользователя вернуть <div>
     return <div>
         {props.usersPage.users.map(u => <div key={u.id}>
         <span>
             <div>
-                <img src={ u.photos.small != null ? u.photos.small : }/>
+                <img src={ u.photos.small != null ? u.photos.small : userPhoto}/>
             </div>
             <div>
                 {u.followed ? <Button color={'default'} onClick={() => {
