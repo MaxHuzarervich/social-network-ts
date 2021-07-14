012b6ad2d0c701b00aea0861fa59775ad6da352f
MaxHuzarevich <pinsk.maks@mail.ru>
2021-07-14 14:51:41 +0300

    53 class component completed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,19 +5,22 @@ import {NavLink} from 'react-router-dom';
 
 function Navbar() {
     return <nav className={s.nav}>
-            <div className={s.item}>
-                <NavLink to='/profile' activeClassName={s.active}>Profile</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to='/dialogs' activeClassName={s.active}>Messages</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to='/news' activeClassName={s.active}>News</NavLink></div>
-            <div className={s.item}>
-                <NavLink to='/music' activeClassName={s.active}>Music</NavLink></div>
-            <div className={s.item}>
-                <NavLink to='/setting' activeClassName={s.active}>Setting</NavLink></div>
-        </nav>
+        <div className={s.item}>
+            <NavLink to='/profile' activeClassName={s.active}>Profile</NavLink>
+        </div>
+        <div className={s.item}>
+            <NavLink to='/dialogs' activeClassName={s.active}>Messages</NavLink>
+        </div>
+        <div className={s.item}>
+            <NavLink to='/users' activeClassName={s.active}>Users</NavLink>
+        </div>
+        <div className={s.item}>
+            <NavLink to='/news' activeClassName={s.active}>News</NavLink></div>
+        <div className={s.item}>
+            <NavLink to='/music' activeClassName={s.active}>Music</NavLink></div>
+        <div className={s.item}>
+            <NavLink to='/setting' activeClassName={s.active}>Setting</NavLink></div>
+    </nav>
 
 }
 
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,38 +1,42 @@
 import React from "react";
-import {UsersPropsType} from "./UsersContainer";
 import {Button} from "@material-ui/core";
 import './Users.module.css';
 import axios from 'axios';
 import userPhoto from '../../assets/images/user.png'
+import {UsersPropsType} from "./UsersContainer";
 
 
-export let Users = (props: UsersPropsType) => {
-    let getUsers = () => {if (props.usersPage.users.length === 0) {
+export class Users extends React.Component<UsersPropsType, any>{
+
+    constructor(props:UsersPropsType) {
+        super(props);
+    }
+    componentDidMount() {
         //когда выполнишь запрос, выполни затем вот этот коллбек(response-ответ)
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             debugger
-            props.setUser(response.data.items)
+            this.props.setUser(response.data.items)
         });
+    }
 
-    }}
-    // для каждого пользователя вернуть <div>
-    return <div className='User-Img'>
-        <button onClick={getUsers}>GetUsers</button>
-        {props.usersPage.users.map(u => <div key={u.id}>
-        <div>
-            <div>
-                <img src={u.photos.small != null ? u.photos.small : userPhoto}/>
-            </div>
-            <div>
-                {u.followed ? <Button color={'default'} onClick={() => {
-                        props.unfollow(u.id)
-                    }}>Unfollow</Button> :
-                    <Button color={'default'} onClick={() => {
-                        props.follow(u.id)
-                    }}>Follow</Button>}
-            </div>
-        </div>
-            <span>
+
+    render() {
+        return <div className='User-Img'>
+            {this.props.usersPage.users.map(u => <div key={u.id}>
+                <div>
+                    <div>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto}/>
+                    </div>
+                    <div>
+                        {u.followed ? <Button color={'default'} onClick={() => {
+                                this.props.unfollow(u.id)
+                            }}>Unfollow</Button> :
+                            <Button color={'default'} onClick={() => {
+                                this.props.follow(u.id)
+                            }}>Follow</Button>}
+                    </div>
+                </div>
+                <span>
             <span>
                 <div>{u.name}</div>
                 <div>{u.status}</div>
@@ -42,6 +46,7 @@ export let Users = (props: UsersPropsType) => {
                 <div>{'u.location.city'}</div>
             </span>
         </span>
-        </div>)}
-    </div>
+            </div>)}
+        </div>
+    }
 }
