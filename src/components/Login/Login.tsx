5fc25dba4aba8e8bc6f0151d61dfb9ef71a3674d
MaxHuzarevich <pinsk.maks@mail.ru>
2021-09-27 13:44:05 +0300

    75 redux form installed

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,27 @@
 import React from "react";
 
 
+export const LoginForm = () => {
+    return (
+        <form>
+            <div>
+                <input placeholder={'Login'}/>
+            </div>
+            <div>
+                <input placeholder={'Password'}/>
+            </div>
+            <div>
+                <input type={'checkbox'}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
 export const Login = () => {
-    return <h1> Login </h1>
+    return <div>
+        <h1>Login</h1>
+        <LoginForm/>
+    </div>
 }
