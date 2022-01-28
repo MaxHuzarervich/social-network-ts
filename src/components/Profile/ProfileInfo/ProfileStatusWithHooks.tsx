d9d4da2360176a173a32794d4029474d68c2ebb3
MaxHuzarevich <pinsk.maks@mail.ru>
2022-01-28 18:50:01 +0300

    add useEffect

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,26 +1,45 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 type ProfileStatusType = {
     status: string,
-    updateStatus: (status:string) => void
+    updateStatus: (status: string) => void
 }
 
-export const ProfileStatusWithHooks = (props:ProfileStatusType) => {
+export const ProfileStatusWithHooks = (props: ProfileStatusType) => {
 
-        let StateWithSetState = useState(false)
-        let editMode = StateWithSetState[0]
-        let setEditMode = StateWithSetState[1]
+    let [editMode, setEditMode] = useState(false)
 
-        return (
-            <>
-                {!editMode &&
-                <div>
-                    <span>{props.status || '-----'}</span>
-                </div>}
-                {editMode &&
-                <div>
-                    <input autoFocus={true}/>
-                </div>}
-            </>
-        )
+    let [status, setStatus] = useState(props.status)
+
+    useEffect( () => {
+        setStatus(props.status)
+    },[props.status])
+
+    const activateMode = () => {
+        setEditMode(true)
+    }
+    const deactivateActiveMode = () => {
+        setEditMode(false)
+        props.updateStatus(status)
+    }
+    const onStatusChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setStatus(e.currentTarget.value)
+    }
+
+    return (
+        <>
+            {!editMode &&
+            <div>
+                <span onDoubleClick={activateMode}>{props.status || '-----'}</span>
+            </div>}
+            {editMode &&
+            <div>
+                <input
+                    value={status}
+                    onChange={onStatusChange}
+                    autoFocus={true}
+                    onBlur={deactivateActiveMode}/>
+            </div>}
+        </>
+    )
 }
