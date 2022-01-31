1413ce722ac668225fa7ea718c6da636ef9d2c4d
MaxHuzarevich <pinsk.maks@mail.ru>
2022-01-31 18:17:43 +0300

    React.memo

--- src/components/Profile/Mypost/MyPosts.tsx
+++ src/components/Profile/Mypost/MyPosts.tsx
@@ -9,40 +9,34 @@ import {TextArea} from "../../common/FormControl/FormControls";
 
 const maxLength10 = maxLengthCreator(10)
 
-class MyPosts extends React.Component<MyPostsPropsType> {
-
-    // shouldComponentUpdate(nextProps: Readonly<MyPostsPropsType>, nextState: Readonly<{}>, nextContext: any): boolean {
-    //     return nextProps != this.props || nextState != this.state
-    // } //избегаем лишней перерисовки
-
-    render() {
-        //значение переменной postsElements будет равно промапленному массиву объектов posts
-        let postsElements = this.props.posts.map((posts: any) => {
-            return (
-                <Post
-                    key={posts.id}
-                    message={posts.message}
-                    likesCount={posts.likesCount}
-                    id={posts.id}
-                />
-            )
-        })
-        const AddNewPost = (values: AddPostFormType) => {
-            this.props.addPost(values.messageForNewPost)
-        }
-        return <div className={s.content}>
-            <div className={s.postBlock}>
-                <h3>My posts</h3>
-                <div>
-                    <AddPostFormRedux onSubmit={AddNewPost}/>
-                </div>
-                <div className={s.posts}>
-                    {postsElements}
-                </div>
+const MyPosts = React.memo((props: MyPostsPropsType) => {
+    console.log('Render!')
+    //значение переменной postsElements будет равно промапленному массиву объектов posts
+    let postsElements = props.posts.map((posts: any) => {
+        return (
+            <Post
+                key={posts.id}
+                message={posts.message}
+                likesCount={posts.likesCount}
+                id={posts.id}
+            />
+        )
+    })
+    const AddNewPost = (values: AddPostFormType) => {
+        props.addPost(values.messageForNewPost)
+    }
+    return <div className={s.content}>
+        <div className={s.postBlock}>
+            <h3>My posts</h3>
+            <div>
+                <AddPostFormRedux onSubmit={AddNewPost}/>
+            </div>
+            <div className={s.posts}>
+                {postsElements}
             </div>
         </div>
-    }
-}
+    </div>
+})
 
 //-------------------------------
 type AddPostFormType = {
